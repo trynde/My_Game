6c020b79eb9c20652e07fac2081fc0ff83dd9a67
trynde <tjoao9906@gmail.com>
2023-05-03 21:11:32 -0300

    arroz

--- js/crud.js
+++ js/crud.js
@@ -1,17 +1,50 @@
 document.querySelector("#salvar").addEventListener("click", cadastar)
 
+let tarefas = []
+
+window.addEventListener("load", () => {
+    tarefas = JSON.parse( localStorage.getItem("tarefas") ) || []
+    atualizar()
+} )
+
+function atualizar(){
+    document.querySelector("#tarefas").innerHTML = " "
+    tarefas.forEach((tarefa) => document.querySelector("#tarefas").innerHTML += criarcard(tarefa))
+}
+
 function cadastar(){
     const titulo = document.querySelector("#titulo").value
     const pontos = document.querySelector("#pontos").value
     const categoria = document.querySelector("#categoria").value
+    const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
 
-    const tarefa = {
+    const tarefa = { //Json java script notation
         titulo,
         pontos,
         categoria
     }
 
-    document.querySelector("#tarefas").innerHTML += criarcard(tarefa)
+    if (!isValid(tarefa.titulo,  document.querySelector("#titulo"))) return
+    if (!isValid(tarefa.pontos, document.querySelector("#pontos"))) return
+
+    tarefas.push(tarefa)
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    
+    atualizar()
+
+    modal.hide()
+}
+
+function isValid(valor, campo){
+    if(valor.length == 0){
+        campo.classList.add("is-invalid")
+        campo.classList.remove("is-valid")
+        return false
+    }else{
+        campo.classList.add("is-valid")
+        campo.classList.remove("is-invalid")
+        return true
+    }
 }
 
 function apagar(botao){
